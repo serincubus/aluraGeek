e3de8c7d3474898afea0df683fb9bda5ea37b595
serincubus <serincubus2@gmail.com>
2024-11-28 23:55:12 -0300

    se realizo archivo eliminarProducto.js y se edito style.css con respecto a la clase div__productos

---
+++ js/eliminarProducto.js
@@ -0,0 +1,24 @@
+import { conexionApi } from "./conexionApi.js";
+ async function eliminarProducto(id) { 
+    try { const respuesta = await fetch(`http://localhost:3001/products/${id}`, {
+         method: 'DELETE', 
+         headers: { 'Content-Type': 'application/json' } });
+
+          if (!respuesta.ok) { 
+            throw new Error('Error al eliminar el producto');
+         } alert('Producto eliminado exitosamente');
+
+          location.reload(); // Recargar la página para reflejar los cambios
+
+          } catch (error) { 
+            console.error("Error al eliminar producto:", error);
+             alert('Hubo un error al eliminar el producto');
+             } } 
+             
+             document.addEventListener('click', function (evento) {
+                 
+                if (evento.target && evento.target.matches('img[data-eliminar]')) {
+                     
+                    const idProducto = evento.target.getAttribute('data-id'); 
+                     eliminarProducto(idProducto);
+                     } });
--- js/mostrarProductos.js
+++ js/mostrarProductos.js
@@ -1,14 +1,14 @@
 import { conexionApi } from "./conexionApi.js";
 const lista = document.querySelector("[data-lista]");
 
-function crearCard(nombre, precio, imagen){
+function crearCard(nombre, precio, imagen, id){
     const contenProducto = document.createElement('div');
     contenProducto.className='contenedor__producto';
     contenProducto.innerHTML=`<img class="imagen__producto" src="/public/images/${imagen}" alt="tasa stormtrooper">
                 <p class="nombre__producto">${nombre}</p>
                 <div class="p_e_icono">
                     <p class="precio__producto">$ ${precio}</p>
-                    <img src="/public/images/🦆 icon _trash.png" alt="">
+                    <img src="/public/images/🦆 icon _trash.png" alt="eliminar" data-eliminar data-id="${id}">
                 </div>`;
 
         return contenProducto;        
@@ -17,7 +17,7 @@ function crearCard(nombre, precio, imagen){
 async function listProductos() { 
     try { const listApi = await conexionApi.listarProductos();
          // Espera la promesa 
-         listApi.forEach(product => lista.appendChild(crearCard(product.nombre, product.precio, product.imagen)));
+         listApi.forEach(product => lista.appendChild(crearCard(product.nombre, product.precio, product.imagen, product.id)));
          } catch (error) { 
             console.error("Error al listar productos:", error);
          } 
